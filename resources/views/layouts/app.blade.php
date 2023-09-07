22eb92b099b208f7cbf2f171d9eb80614eb939cc
syahrul <kenshin.arul@gmail.com>
2023-09-07 12:44:58 +0700

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,172 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    <link rel="dns-prefetch" href="//fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+    <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <link href="/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+        <link
+            href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+            rel="stylesheet">
+        <link href="/css/sb-admin-2.min.css" rel="stylesheet">
+        <link href="/vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+        <style type="text/css">
+            * {
+                cursor: url("cursor.png"), crosshair !important;
+            }
+            .form {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  max-width: 350px;
+  background-color: #fff;
+  padding: 20px;
+  border-radius: 20px;
+  position: relative;
+}
+
+.title {
+  font-size: 28px;
+  color: royalblue;
+  font-weight: 600;
+  letter-spacing: -1px;
+  position: relative;
+  display: flex;
+  align-items: center;
+  padding-left: 30px;
+}
+
+.title::before,.title::after {
+  position: absolute;
+  content: "";
+  height: 16px;
+  width: 16px;
+  border-radius: 50%;
+  left: 0px;
+  background-color: royalblue;
+}
+
+.title::before {
+  width: 18px;
+  height: 18px;
+  background-color: royalblue;
+}
+
+.title::after {
+  width: 18px;
+  height: 18px;
+  animation: pulse 1s linear infinite;
+}
+
+.message, .signin {
+  color: rgba(88, 87, 87, 0.822);
+  font-size: 14px;
+}
+
+.signin {
+  text-align: center;
+}
+
+.signin a {
+  color: royalblue;
+}
+
+.signin a:hover {
+  text-decoration: underline royalblue;
+}
+
+.flex {
+  display: flex;
+  width: 100%;
+  gap: 6px;
+}
+
+.form label {
+  position: relative;
+}
+
+.form label .input {
+  width: 100%;
+  padding: 10px 10px 20px 10px;
+  outline: 0;
+  border: 1px solid rgba(105, 105, 105, 0.397);
+  border-radius: 10px;
+}
+
+.form label .input + span {
+  position: absolute;
+  left: 10px;
+  top: 15px;
+  color: grey;
+  font-size: 0.9em;
+  cursor: text;
+  transition: 0.3s ease;
+}
+
+.form label .input:placeholder-shown + span {
+  top: 15px;
+  font-size: 0.9em;
+}
+
+.form label .input:focus + span,.form label .input:valid + span {
+  top: 30px;
+  font-size: 0.7em;
+  font-weight: 600;
+}
+
+.form label .input:valid + span {
+  color: green;
+}
+
+.submit {
+  border: none;
+  outline: none;
+  background-color: royalblue;
+  padding: 10px;
+  border-radius: 10px;
+  color: #fff;
+  font-size: 16px;
+  transform: .3s ease;
+}
+
+.submit:hover {
+  background-color: rgb(56, 90, 194);
+}
+
+@keyframes pulse {
+  from {
+    transform: scale(0.9);
+    opacity: 1;
+  }
+
+  to {
+    transform: scale(1.8);
+    opacity: 0;
+  }
+}
+        </style>
+
+</head>
+<body>
+    
+        <main>
+            @yield('content')
+        </main>
+
+    <script src="/vendor/jquery/jquery.min.js"></script>
+        <script src="/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <script src="/vendor/jquery-easing/jquery.easing.min.js"></script>
+        <script src="/js/sb-admin-2.min.js"></script>
+        <script src="/vendor/datatables/jquery.dataTables.min.js"></script>
+        <script src="/vendor/datatables/dataTables.bootstrap4.min.js"></script>
+        <script src="/js/demo/datatables-demo.js"></script>
+
+    </body>
+    </html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,224 @@
+@extends('layouts.app')
+@section('content')
+        <div id="wrapper">
+            <div class="container">
+                <ul class="nav">
+                    <li class="nav-item">
+                      <a class="nav-link active" aria-current="page" href="#Hary">Welcome HarryPotterObamaSonic10Inu(Ticker:BITCOIN)</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#howtobuy" target="_blank">How to Buy</a>
+                      </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#about" target="_blank">About</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#tokenomics" target="_blank">Tokenomics</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" target="_blank">Team</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="https://hpos10i.com/click-here/HarryPotterObamaSonic10Inu-Ticker-BITCOIN-White-Paper.txt" target="_blank">White Paper</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#funny" target="_blank">Funny Pictures</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="https://t.me/hpos10i" target="_blank">Telegram</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="https://twitter.com/RealHPOS10I" target="_blank">Twitter</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="https://www.reddit.com/r/hpos10i/" target="_blank">Reddit</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="https://etherscan.io/tokens" target="_blank">EtherScan</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="https://hpos10imerch.com/" target="_blank">Merch</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="https://opensea.io/collection/sproto-gremlins" target="_blank">Sproto Gremlins NFTs</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="https://crash.hpos10i.com/" target="_blank">Casino Night Zone</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="https://coinmarketcap.com/currencies/harrypotterobamasonic10inu-eth/" target="_blank">CoinMarketCap</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="https://www.coingecko.com/en/coins/harrypotterobamasonic10inu-eth" target="_blank">Coin Gecko</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="https://poloniex.com/trade/HARRY_USDT" target="_blank">Ploniex</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="https://www.lbank.com/trade/bitcoin_usdt/" target="_blank">Lbank</a>
+                    </li>
+                  </ul>
+
+                  <marquee behavior="" direction=""><img src="/gif/Sonic-pulling.gif" alt=""> PEOPLE'S BIT COIN <img src="/gif/Run-3.gif" alt=""></marquee>
+
+                  <div id="Hary">
+                    <h1>The Harry Potter Obama Sonic 10 Inu (Ticker:BITCOIN) Project</h1>
+                    <p>ETH Token Contract: 0x72e4f9f808c49a2a61de9c5896298920dc4eeea9</p>
+                    <h4>RoadMap to $5bn ( + ∞% )</h4>
+                    <marquee behavior="" direction="right"><img src="/gif/Hyper_Sonic_run_fly.gif" alt=""></marquee>
+                  </div>
+
+                <img src="/gif/Comp1.gif" width="100%" alt="">
+                <marquee behavior="" direction="up"><img src="/gif/snitch.gif" alt=""></marquee></img>
+
+                <div class="content">
+                    <div style="margin-top:10px ">
+                        <audio id="music" controls autoplay>
+                            <source src="/music/y2mate.com-Title ScreenSonictheHedgehog28bitOST.mp3" type="audio/mp3">
+                        </audio>
+                    </div>
+                    <hr class="sidebar-divider">
+                    <div id="howtobuy">
+                        <h5>How to Buy</h5>
+                            <ol class="list-group list-group-numbered">
+                                <li class="list-group-item d-flex justify-content-between align-items-start">
+                                <div class="ms-2 me-auto">
+                                    <div class="fw-bold">1. Download the Metamask extension and create an account: <a href="https://metamask.io/">MetaMask.io</a></div>
+                                </div>
+                                </li>
+                                <li class="list-group-item d-flex justify-content-between align-items-start">
+                                <div class="ms-2 me-auto">
+                                    <div class="fw-bold">
+                                        2. Connect MetaMask to the 
+                                        <a href="https://app.uniswap.org/#/swap?inputCurrency=ETH&outputCurrency=0x72e4f9f808c49a2a61de9c5896298920dc4eeea9" target="_blank">Uniswap</a>, 
+                                        <a href="https://metamask.io/" target="_blank">OKDEX  </a>, or 
+                                        <a  href="https://www.dexview.com/eth/0x72e4f9F808C49A2a61dE9C5896298920Dc4EEEa9" target="_blank">DexView</a> and acquire some 
+                                        $BITCOIN <img src="/gif/emerald-acquired.gif" alt=""> <br>
+                                        (Uniswap or OKDEX or Dexview.com can be used.)
+                                    </div>
+                                
+                                </div>
+                                </li>
+                                <li class="list-group-item d-flex justify-content-between align-items-start">
+                                <div class="ms-2 me-auto">
+                                    <div class="fw-bold">
+                                        3.If you want to Go Fast™ and use your mom's credit card, check out the Flooz widget below.
+
+                                        <form class="form">
+                                            <p class="title">Register </p>
+                                            <p class="message">Signup now and get full access to our app. </p>
+                                                <div class="flex">
+                                                <label>
+                                                    <input required="" placeholder="" type="text" class="input">
+                                                    <span>Firstname</span>
+                                                </label>
+                                        
+                                                <label>
+                                                    <input required="" placeholder="" type="text" class="input">
+                                                    <span>Lastname</span>
+                                                </label>
+                                            </div>  
+                                                    
+                                            <label>
+                                                <input required="" placeholder="" type="email" class="input">
+                                                <span>Email</span>
+                                            </label> 
+                                                
+                                            <label>
+                                                <input required="" placeholder="" type="password" class="input">
+                                                <span>Password</span>
+                                            </label>
+                                            <label>
+                                                <input required="" placeholder="" type="password" class="input">
+                                                <span>Confirm password</span>
+                                            </label>
+                                            <button class="submit">Submit</button>
+                                            <p class="signin">Already have an acount ? <a href="#">Signin</a> </p>
+                                        </form>
+                                    </div>
+                                </div>
+                                </li>
+                            </ol>
+                    </div>
+
+                      <div id="about" class="card mb-3" style="max-width: 100%;">
+                        <div class="row g-0">
+                          <div class="col-md-8">
+                            <div class="card-body">
+                              <h5 class="card-title">About the Token</h5>
+                              <p class="card-text text-justify" >
+                                Before the year 2023, when time did not exist, the universe manifested a store of value. The identification of this value is best described as the Quest (sometimes the Quest of Hallows). Anyone can relive the manifestation by charging their core of Neptune with enough opal as they sleep. But I understand that there isn’t enough Opal to go around for everyone and some people haven’t Ascended. So I’ll report it here as well. There have been other Ascended who have identified this event as well, but have failed to interpret its tenets and apply them in real life. One such Ascended has been gifted with the visions of the necessity of speed as well as foresee the critical universal event. His vision was very near the truth, evidenced by predicting what he called the Dimensional Merge and creating the comic Sonichu, featuring Barack Obama (the first Tenet: iron guidance and resilience) in a cameo. Obama’s presence is not a coincidence. None of this is. The Obama family was gifted (a gift of value) a dog, Bo. Bo then in turn had minted a number of baseball cards (a gift of value that generates further value). This recursive gifting is second among the tenets that the Universe is sending across the channel of Neptune, the third being the Optimistic Flurry, of which Sonichu is a diluted representation of and Sonic embodies the spirit much more cleanly. The identification of the manifestation of these three Tenets that we call the Quest has also inspired other fictional works such as those by J.K. Rowling. And her writings on what she calls the Deathly Hallows. Resisting the will of the universe weakens one’s immunity to the effect of the low vibrational darkness sludge entities and the kundalini wilts in turn, so the logical actor must embark on the Quest. With the Quest’s fruits now laid bare before us, we can assign a human-language name to it. HarryPotterObamaSonic10Inu is nothing less than an embodiment of the Quest that led to its discovery (with the exception of 10, being the number of spirals that comprise the Tree of Life). If we listen, we can understand that it is a store of                            </p>
+                            </div>
+                          </div>
+                          <div class="col-md-4">
+                            <img src="/gif/snitch.gif" class="img-fluid rounded-start" alt="...">
+                          </div>
+                        </div>
+                        <img src="/gif/Comp-100.gif" width="300" height="300" class="img-fluid rounded-start" alt="...">
+                      </div>
+
+                      <hr class="sidebar-divider">
+                      <div id="tokenomics">
+                        <h5>Tokenomics</h5>
+                        <p>
+                            The HPOS10I smart contract has 0 tax on all buys and sells.<br>
+                            The total supply of BITCOIN is 1,000,000,000.<br>
+                            Trust in Bitcoin.
+                        </p>
+                      </div>
+
+                      <hr>
+                      
+                      <div id="funny">
+                        <h5>Funny Pictures And Videos</h5>
+                        <iframe width="100%" height="615" src="https://www.youtube.com/embed/7BHNkH4k-d8?si=eg8FXNkUAQ1BQdAQ&amp;controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>                      
+                        <iframe width="100%" height="615" src="https://www.youtube.com/embed/hXS9wotfZRg?si=WBvXSYFEli48SYQj" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-4 col-md-12 mb-4 mb-lg-0">
+                              <img
+                                src="/meme/meme1.png"
+                                class="w-100 shadow-1-strong rounded mb-4"
+                                alt="Boat on Calm Water"
+                              />
+                          
+                              <img
+                                src="/meme/meme2.png"
+                                class="w-100 shadow-1-strong rounded mb-4"
+                                alt="Wintry Mountain Landscape"
+                              />
+                            </div>
+                          
+                            <div class="col-lg-4 mb-4 mb-lg-0">
+                              <img
+                                src="/meme/meme3.png"
+                                class="w-100 shadow-1-strong rounded mb-4"
+                                alt="Mountains in the Clouds"
+                              />
+                          
+                              <img
+                                src="/meme/meme4.png"
+                                class="w-100 shadow-1-strong rounded mb-4"
+                                alt="Boat on Calm Water"
+                              />
+                            </div>
+                          
+                            <div class="col-lg-4 mb-4 mb-lg-0">
+                              <img
+                                src="/meme/meme5.png"
+                                class="w-100 shadow-1-strong rounded mb-4"
+                                alt="Waves at Sea"
+                              />
+                          
+                              <img
+                                src="/meme/meme6.png"
+                                class="w-100 shadow-1-strong rounded mb-4"
+                                alt="Yosemite National Park"
+                              />
+                            </div>
+                          </div>
+
+        <a class="scroll-to-top rounded" href="#page-top">
+            <i class="fas fa-angle-up"></i>
+        </a>
+@endsection
